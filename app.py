0ebc987ae2a8567e6873e9fea76ea2b286b39b51
Gaetan Manchon <gmanchon@gmail.com>
2021-08-27 16:33:49 +0200

    write credentials file from env var

--- app.py
+++ app.py
@@ -3,12 +3,27 @@ import streamlit as st
 
 import pandas as pd
 
+import os
+
 from google.cloud import storage
 
 BUCKET_NAME = "le-wagon-data"
 storage_filename = "data/train_1k.csv"
 local_filename = "train_1k_downloaded.csv"
 upload_storage_filename = "data/train_1k_uploaded.csv"
+
+# create credentials file
+google_credentials_file = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+
+if not os.path.isfile(google_credentials_file):
+
+    # retrieve credentials
+    json_credentials = os.environ["GOOGLE_CREDS"]
+
+    # write credentials
+    with open(google_credentials_file, "w") as file:
+
+        file.write(json_credentials)
 
 # download file
 client = storage.Client()
