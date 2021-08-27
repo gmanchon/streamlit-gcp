23c14c13bae775a0988928d6237121ddf629f0de
Gaetan Manchon <gmanchon@gmail.com>
2021-08-27 15:54:23 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+
+from google.cloud import storage
+
+BUCKET_NAME = "le-wagon-data"
+storage_filename = "data/train_1k.csv"
+local_filename = "train_1k_downloaded.csv"
+upload_storage_filename = "data/train_1k_uploaded.csv"
+
+# download file
+client = storage.Client()
+bucket = client.bucket(BUCKET_NAME)
+blob = bucket.blob(storage_filename)
+blob.download_to_filename(local_filename)
+
+# df
+df = pd.read_csv(local_filename)
+df
+
+# upload file
+upload_blob = bucket.blob(upload_storage_filename)
+upload_blob.upload_from_filename(local_filename)
